709c43333fea270f7ed1d811f2ce613ffdbfd91a
Kyle Barrett <kyle@squaretwo.co>
2020-09-23 11:02:15 -0500

    Remove all dependencies.

--- src/android/AppLovinMaxInterstitialAd.java
+++ src/android/AppLovinMaxInterstitialAd.java
@@ -1,177 +1,177 @@
-package com.lakgames.plugins.applovinmax;
-
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.os.Handler;
-
-import com.applovin.mediation.MaxAd;
-import com.applovin.mediation.MaxAdListener;
-import com.applovin.mediation.ads.MaxInterstitialAd;
-
-public class AppLovinMaxInterstitialAd implements MaxAdListener {
-
-    private static final String VIDEO_LOADED_EVENT = "interstitialVideoLoaded";
-    private static final String VIDEO_LOADING_FAILED_EVENT = "interstitialVideoLoadingFailed";
-
-    private static final String VIDEO_FAILED_DISPLAY_EVENT = "interstitialVideoDisplayFailed";
-    private static final String VIDEO_START_DISPLAY_EVENT = "interstitialVideoDisplayStart";
-
-    private static final String VIDEO_ENDED_HIDDEN = "interstitialVideoHidden";
-
-    private static final String VIDEO_CLICKED = "interstitialVideoClicked";
-
-    private AppLovinMaxPlugin plugin;
-    private boolean isInit = false;
-    private MaxInterstitialAd interstitialAd;
-    private int retryAttempt;
-
-    public AppLovinMaxInterstitialAd(AppLovinMaxPlugin plugin) {
-        this.plugin = plugin;
-    }
-
-    public void createInterstitialAd(String unitId) {
-        if(isInit){
-            return;
-        }
-        interstitialAd = new MaxInterstitialAd(unitId, plugin.cordova.getActivity());
-        interstitialAd.setListener(this);
-        interstitialAd.loadAd();
-        isInit = true;
-    }
-
-    public void showInterstitialVideo(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if(!isInit){
-            return;
-        }
-        String placement = args.getString(0);
-        plugin.cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                if (!interstitialAd.isReady()){
-                    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, false));
-                }else{
-                    interstitialAd.showAd(placement);
-                    callbackContext.success();
-                }
-            }
-        });
-    }
-
-    public void hasInterstitialVideo(JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if(!isInit){
-            return;
-        }
-        plugin.cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, interstitialAd.isReady()));
-            }
-        });
-    }
-
-    // MAX Ad Listener
-    @Override
-    public void onAdLoaded(final MaxAd maxAd) {
-        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
-        // Reset retry attempt
-        retryAttempt = 0;
-        JSONObject data = new JSONObject();
-        try{
-            data.put("unitId", maxAd.getAdUnitId());
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-        this.emitWindowEvent(VIDEO_LOADED_EVENT, data);
-    }
-
-    @Override
-    public void onAdLoadFailed(final String adUnitId, final int errorCode) {
-        // Interstitial ad failed to load. We recommend retrying with exponentially higher delays.
-        retryAttempt++;
-        new Handler().postDelayed( new Runnable() {
-            @Override
-            public void run() { interstitialAd.loadAd(); }
-        }, 3000 );
-        JSONObject data = new JSONObject();
-        try{
-            data.put("unitId", adUnitId);
-            data.put("errorCode", errorCode);
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-        this.emitWindowEvent(VIDEO_LOADING_FAILED_EVENT, data);
-    }
-
-    @Override
-    public void onAdDisplayFailed(final MaxAd maxAd, final int errorCode) {
-        // Interstitial ad failed to display. We recommend loading the next ad
-        interstitialAd.loadAd();
-        JSONObject data = new JSONObject();
-        try{
-            data.put("unitId", maxAd.getAdUnitId());
-            data.put("errorCode", errorCode);
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-        this.emitWindowEvent(VIDEO_FAILED_DISPLAY_EVENT, data);
-    }
-
-    @Override
-    public void onAdDisplayed(final MaxAd maxAd) {
-        JSONObject data = new JSONObject();
-        try{
-            data.put("unitId", maxAd.getAdUnitId());
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-        this.emitWindowEvent(VIDEO_START_DISPLAY_EVENT, data);
-    }
-
-    @Override
-    public void onAdClicked(final MaxAd maxAd) {
-        JSONObject data = new JSONObject();
-        try{
-            data.put("unitId", maxAd.getAdUnitId());
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-        this.emitWindowEvent(VIDEO_CLICKED, data);
-    }
-
-    @Override
-    public void onAdHidden(final MaxAd maxAd) {
-        // Interstitial ad is hidden. Pre-load the next ad
-        interstitialAd.loadAd();
-        JSONObject data = new JSONObject();
-        try{
-            data.put("unitId", maxAd.getAdUnitId());
-        }catch(JSONException e){
-            e.printStackTrace();
-        }
-        this.emitWindowEvent(VIDEO_ENDED_HIDDEN, data);
-    }
-
-    /** ----------------------- UTILS --------------------------- */
-    private void emitWindowEvent(final String event) {
-        final CordovaWebView view = plugin.getWebView();
-        plugin.cordova.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.loadUrl(String.format("javascript:cordova.fireWindowEvent('%s');", event));
-            }
-        });
-    }
-
-    private void emitWindowEvent(final String event, final JSONObject data) {
-        final CordovaWebView view = plugin.getWebView();
-        plugin.cordova.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                view.loadUrl(String.format("javascript:cordova.fireWindowEvent('%s', %s);", event, data.toString()));
-            }
-        });
-    }
-}
+// package com.lakgames.plugins.applovinmax;
+//
+// import org.apache.cordova.CallbackContext;
+// import org.apache.cordova.CordovaWebView;
+// import org.apache.cordova.PluginResult;
+// import org.json.JSONArray;
+// import org.json.JSONException;
+// import org.json.JSONObject;
+//
+// import android.os.Handler;
+//
+// import com.applovin.mediation.MaxAd;
+// import com.applovin.mediation.MaxAdListener;
+// import com.applovin.mediation.ads.MaxInterstitialAd;
+//
+// public class AppLovinMaxInterstitialAd implements MaxAdListener {
+//
+//     private static final String VIDEO_LOADED_EVENT = "interstitialVideoLoaded";
+//     private static final String VIDEO_LOADING_FAILED_EVENT = "interstitialVideoLoadingFailed";
+//
+//     private static final String VIDEO_FAILED_DISPLAY_EVENT = "interstitialVideoDisplayFailed";
+//     private static final String VIDEO_START_DISPLAY_EVENT = "interstitialVideoDisplayStart";
+//
+//     private static final String VIDEO_ENDED_HIDDEN = "interstitialVideoHidden";
+//
+//     private static final String VIDEO_CLICKED = "interstitialVideoClicked";
+//
+//     private AppLovinMaxPlugin plugin;
+//     private boolean isInit = false;
+//     private MaxInterstitialAd interstitialAd;
+//     private int retryAttempt;
+//
+//     public AppLovinMaxInterstitialAd(AppLovinMaxPlugin plugin) {
+//         this.plugin = plugin;
+//     }
+//
+//     public void createInterstitialAd(String unitId) {
+//         if(isInit){
+//             return;
+//         }
+//         interstitialAd = new MaxInterstitialAd(unitId, plugin.cordova.getActivity());
+//         interstitialAd.setListener(this);
+//         interstitialAd.loadAd();
+//         isInit = true;
+//     }
+//
+//     public void showInterstitialVideo(JSONArray args, final CallbackContext callbackContext) throws JSONException {
+//         if(!isInit){
+//             return;
+//         }
+//         String placement = args.getString(0);
+//         plugin.cordova.getActivity().runOnUiThread(new Runnable() {
+//             public void run() {
+//                 if (!interstitialAd.isReady()){
+//                     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, false));
+//                 }else{
+//                     interstitialAd.showAd(placement);
+//                     callbackContext.success();
+//                 }
+//             }
+//         });
+//     }
+//
+//     public void hasInterstitialVideo(JSONArray args, final CallbackContext callbackContext) throws JSONException {
+//         if(!isInit){
+//             return;
+//         }
+//         plugin.cordova.getActivity().runOnUiThread(new Runnable() {
+//             public void run() {
+//                 callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, interstitialAd.isReady()));
+//             }
+//         });
+//     }
+//
+//     // MAX Ad Listener
+//     @Override
+//     public void onAdLoaded(final MaxAd maxAd) {
+//         // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
+//         // Reset retry attempt
+//         retryAttempt = 0;
+//         JSONObject data = new JSONObject();
+//         try{
+//             data.put("unitId", maxAd.getAdUnitId());
+//         }catch(JSONException e){
+//             e.printStackTrace();
+//         }
+//         this.emitWindowEvent(VIDEO_LOADED_EVENT, data);
+//     }
+//
+//     @Override
+//     public void onAdLoadFailed(final String adUnitId, final int errorCode) {
+//         // Interstitial ad failed to load. We recommend retrying with exponentially higher delays.
+//         retryAttempt++;
+//         new Handler().postDelayed( new Runnable() {
+//             @Override
+//             public void run() { interstitialAd.loadAd(); }
+//         }, 3000 );
+//         JSONObject data = new JSONObject();
+//         try{
+//             data.put("unitId", adUnitId);
+//             data.put("errorCode", errorCode);
+//         }catch(JSONException e){
+//             e.printStackTrace();
+//         }
+//         this.emitWindowEvent(VIDEO_LOADING_FAILED_EVENT, data);
+//     }
+//
+//     @Override
+//     public void onAdDisplayFailed(final MaxAd maxAd, final int errorCode) {
+//         // Interstitial ad failed to display. We recommend loading the next ad
+//         interstitialAd.loadAd();
+//         JSONObject data = new JSONObject();
+//         try{
+//             data.put("unitId", maxAd.getAdUnitId());
+//             data.put("errorCode", errorCode);
+//         }catch(JSONException e){
+//             e.printStackTrace();
+//         }
+//         this.emitWindowEvent(VIDEO_FAILED_DISPLAY_EVENT, data);
+//     }
+//
+//     @Override
+//     public void onAdDisplayed(final MaxAd maxAd) {
+//         JSONObject data = new JSONObject();
+//         try{
+//             data.put("unitId", maxAd.getAdUnitId());
+//         }catch(JSONException e){
+//             e.printStackTrace();
+//         }
+//         this.emitWindowEvent(VIDEO_START_DISPLAY_EVENT, data);
+//     }
+//
+//     @Override
+//     public void onAdClicked(final MaxAd maxAd) {
+//         JSONObject data = new JSONObject();
+//         try{
+//             data.put("unitId", maxAd.getAdUnitId());
+//         }catch(JSONException e){
+//             e.printStackTrace();
+//         }
+//         this.emitWindowEvent(VIDEO_CLICKED, data);
+//     }
+//
+//     @Override
+//     public void onAdHidden(final MaxAd maxAd) {
+//         // Interstitial ad is hidden. Pre-load the next ad
+//         interstitialAd.loadAd();
+//         JSONObject data = new JSONObject();
+//         try{
+//             data.put("unitId", maxAd.getAdUnitId());
+//         }catch(JSONException e){
+//             e.printStackTrace();
+//         }
+//         this.emitWindowEvent(VIDEO_ENDED_HIDDEN, data);
+//     }
+//
+//     /** ----------------------- UTILS --------------------------- */
+//     private void emitWindowEvent(final String event) {
+//         final CordovaWebView view = plugin.getWebView();
+//         plugin.cordova.getActivity().runOnUiThread(new Runnable() {
+//             @Override
+//             public void run() {
+//                 view.loadUrl(String.format("javascript:cordova.fireWindowEvent('%s');", event));
+//             }
+//         });
+//     }
+//
+//     private void emitWindowEvent(final String event, final JSONObject data) {
+//         final CordovaWebView view = plugin.getWebView();
+//         plugin.cordova.getActivity().runOnUiThread(new Runnable() {
+//             @Override
+//             public void run() {
+//                 view.loadUrl(String.format("javascript:cordova.fireWindowEvent('%s', %s);", event, data.toString()));
+//             }
+//         });
+//     }
+// }
--- www/applovinmax.js
+++ www/applovinmax.js
@@ -1,126 +1,126 @@
-function appLovinMaxExec(name, params, onSuccess, onFailure) {
-    cordova.exec(
-        function callPluginSuccess(result) {
-            if (isFunction(onSuccess)) {
-                onSuccess(result);
-            }
-        },
-        function callPluginFailure(error) {
-            if (isFunction(onFailure)) {
-                onFailure(error)
-            }
-        },
-        'AppLovinMaxPlugin', name, params);
-}
-
-/**
- * Helper function to do a shallow defaults (merge). Does not create a new object, simply extends it
- * @param {Object} o - object to extend
- * @param {Object} defaultObject - defaults to extend o with
- */
-function defaults(o, defaultObject) {
-    if (typeof o === 'undefined') {
-        return defaults({}, defaultObject);
-    }
-    for (var j in defaultObject) {
-        if (defaultObject.hasOwnProperty(j) && o.hasOwnProperty(j) === false) {
-            o[j] = defaultObject[j];
-        }
-    }
-    return o;
-}
-
-/**
- * Helper function to check if a function is a function
- * @param {Object} functionToCheck - function to check if is function
- */
-function isFunction(functionToCheck) {
-    var getType = {};
-    var isFunction = functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-    return isFunction === true;
-}
-
-var isAppLovinMaxInit = false;
-let AppLovinMax = (function (){
-    return {
-        init: function (params) {
-            if(isAppLovinMaxInit){
-                throw new Error("AppLovinMax init action error: plugin already initialized");
-            }
-            isAppLovinMaxInit = true;
-            params = defaults(params, {});
-            appLovinMaxExec('init', [], params.onSuccess, params.onFailure);
-        },
-        initAdUnit : function (params) {
-            if(!isAppLovinMaxInit){
-                throw new Error("AppLovinMax initAdUnit action error: plugin has not initialized");
-            }
-            if (params.hasOwnProperty("unit") === false){
-                throw new Error("AppLovinMax initAdUnit action error: missing unit type [REWARDED, BANNER, INTERSTITIAL]");
-            }
-            var unit = params["unit"];
-            const UNIT_TYPES = ["REWARDED", "BANNER", "INTERSTITIAL"];
-            if(UNIT_TYPES.indexOf(unit) === -1){
-                throw new Error("AppLovinMax initAdUnit action error: unknown unit type [REWARDED, BANNER, INTERSTITIAL]");
-            }
-            if (params.hasOwnProperty("unitId") === false){
-                throw new Error("AppLovinMax initAdUnit action error: missing unitId");
-            }
-            var unitId = params["unitId"];
-            appLovinMaxExec('initAdUnit', [unit, unitId], params.onSuccess, params.onFailure);
-        },
-        setUserId : function (params) {
-            if(!isAppLovinMaxInit){
-                throw new Error("AppLovinMax setUserId action error: plugin has not initialized");
-            }
-            params = defaults(params, {});
-            if (params.hasOwnProperty('userId') === false){
-                throw new Error("AppLovinMax setUserId action error: missing userId");
-            }
-            let userId = params["userId"];
-            appLovinMaxExec('setUserId', [userId], params.onSuccess, params.onFailure);
-        },
-        showRewardedVideo : function(params) {
-            if(!isAppLovinMaxInit){
-                throw new Error("AppLovinMax showRewardedVideo action error: plugin has not initialized");
-            }
-            params = defaults(params, {});
-            !params.hasOwnProperty('placement') ? params["placement"] = "" : 0;
-            let placement = params["placement"];
-            appLovinMaxExec('showRewardedVideo', [placement], params.onSuccess, params.onFailure);
-        },
-        hasRewardedVideo : function (params) {
-            if(!isAppLovinMaxInit){
-                throw new Error("AppLovinMax hasRewardedVideo action error: plugin has not initialized");
-            }
-            params = defaults(params, {});
-            if (isFunction(params.onSuccess) === false) {
-                throw new Error('AppLovinMax hasRewardedVideo action error: missing onSuccess callback');
-            }
-            appLovinMaxExec('hasRewardedVideo', [], params.onSuccess, params.onFailure);
-        },
-        showInterstitialVideo : function(params) {
-            if(!isAppLovinMaxInit){
-                throw new Error("AppLovinMax showInterstitialVideo action error: plugin has not initialized");
-            }
-            params = defaults(params, {});
-            !params.hasOwnProperty('placement') ? params["placement"] = "" : 0;
-            let placement = params["placement"];
-            appLovinMaxExec('showInterstitialVideo', [placement], params.onSuccess, params.onFailure);
-        },
-        hasInterstitialVideo : function (params) {
-            if(!isAppLovinMaxInit){
-                throw new Error("AppLovinMax hasInterstitialVideo action error: plugin has not initialized");
-            }
-            params = defaults(params, {});
-            if (isFunction(params.onSuccess) === false) {
-                throw new Error('AppLovinMax hasInterstitialVideo action error: missing onSuccess callback');
-            }
-            appLovinMaxExec('hasInterstitialVideo', [], params.onSuccess, params.onFailure);
-        }
-    }
-})();
-
-if (typeof module !== undefined && module.exports) {
-    module.exports = AppLovinMax;
-}
+// function appLovinMaxExec(name, params, onSuccess, onFailure) {
+//     cordova.exec(
+//         function callPluginSuccess(result) {
+//             if (isFunction(onSuccess)) {
+//                 onSuccess(result);
+//             }
+//         },
+//         function callPluginFailure(error) {
+//             if (isFunction(onFailure)) {
+//                 onFailure(error)
+//             }
+//         },
+//         'AppLovinMaxPlugin', name, params);
+// }
+//
+// /**
+//  * Helper function to do a shallow defaults (merge). Does not create a new object, simply extends it
+//  * @param {Object} o - object to extend
+//  * @param {Object} defaultObject - defaults to extend o with
+//  */
+// function defaults(o, defaultObject) {
+//     if (typeof o === 'undefined') {
+//         return defaults({}, defaultObject);
+//     }
+//     for (var j in defaultObject) {
+//         if (defaultObject.hasOwnProperty(j) && o.hasOwnProperty(j) === false) {
+//             o[j] = defaultObject[j];
+//         }
+//     }
+//     return o;
+// }
+//
+// /**
+//  * Helper function to check if a function is a function
+//  * @param {Object} functionToCheck - function to check if is function
+//  */
+// function isFunction(functionToCheck) {
+//     var getType = {};
+//     var isFunction = functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+//     return isFunction === true;
+// }
+//
+// var isAppLovinMaxInit = false;
+// let AppLovinMax = (function (){
+//     return {
+//         init: function (params) {
+//             if(isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax init action error: plugin already initialized");
+//             }
+//             isAppLovinMaxInit = true;
+//             params = defaults(params, {});
+//             appLovinMaxExec('init', [], params.onSuccess, params.onFailure);
+//         },
+//         initAdUnit : function (params) {
+//             if(!isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax initAdUnit action error: plugin has not initialized");
+//             }
+//             if (params.hasOwnProperty("unit") === false){
+//                 throw new Error("AppLovinMax initAdUnit action error: missing unit type [REWARDED, BANNER, INTERSTITIAL]");
+//             }
+//             var unit = params["unit"];
+//             const UNIT_TYPES = ["REWARDED", "BANNER", "INTERSTITIAL"];
+//             if(UNIT_TYPES.indexOf(unit) === -1){
+//                 throw new Error("AppLovinMax initAdUnit action error: unknown unit type [REWARDED, BANNER, INTERSTITIAL]");
+//             }
+//             if (params.hasOwnProperty("unitId") === false){
+//                 throw new Error("AppLovinMax initAdUnit action error: missing unitId");
+//             }
+//             var unitId = params["unitId"];
+//             appLovinMaxExec('initAdUnit', [unit, unitId], params.onSuccess, params.onFailure);
+//         },
+//         setUserId : function (params) {
+//             if(!isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax setUserId action error: plugin has not initialized");
+//             }
+//             params = defaults(params, {});
+//             if (params.hasOwnProperty('userId') === false){
+//                 throw new Error("AppLovinMax setUserId action error: missing userId");
+//             }
+//             let userId = params["userId"];
+//             appLovinMaxExec('setUserId', [userId], params.onSuccess, params.onFailure);
+//         },
+//         showRewardedVideo : function(params) {
+//             if(!isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax showRewardedVideo action error: plugin has not initialized");
+//             }
+//             params = defaults(params, {});
+//             !params.hasOwnProperty('placement') ? params["placement"] = "" : 0;
+//             let placement = params["placement"];
+//             appLovinMaxExec('showRewardedVideo', [placement], params.onSuccess, params.onFailure);
+//         },
+//         hasRewardedVideo : function (params) {
+//             if(!isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax hasRewardedVideo action error: plugin has not initialized");
+//             }
+//             params = defaults(params, {});
+//             if (isFunction(params.onSuccess) === false) {
+//                 throw new Error('AppLovinMax hasRewardedVideo action error: missing onSuccess callback');
+//             }
+//             appLovinMaxExec('hasRewardedVideo', [], params.onSuccess, params.onFailure);
+//         },
+//         showInterstitialVideo : function(params) {
+//             if(!isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax showInterstitialVideo action error: plugin has not initialized");
+//             }
+//             params = defaults(params, {});
+//             !params.hasOwnProperty('placement') ? params["placement"] = "" : 0;
+//             let placement = params["placement"];
+//             appLovinMaxExec('showInterstitialVideo', [placement], params.onSuccess, params.onFailure);
+//         },
+//         hasInterstitialVideo : function (params) {
+//             if(!isAppLovinMaxInit){
+//                 throw new Error("AppLovinMax hasInterstitialVideo action error: plugin has not initialized");
+//             }
+//             params = defaults(params, {});
+//             if (isFunction(params.onSuccess) === false) {
+//                 throw new Error('AppLovinMax hasInterstitialVideo action error: missing onSuccess callback');
+//             }
+//             appLovinMaxExec('hasInterstitialVideo', [], params.onSuccess, params.onFailure);
+//         }
+//     }
+// })();
+//
+// if (typeof module !== undefined && module.exports) {
+//     module.exports = AppLovinMax;
+// }
